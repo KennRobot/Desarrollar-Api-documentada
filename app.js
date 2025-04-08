94f4bbd1845a6711f83aed89da85d512ec1a287e
NeoDoyle <otvamu@gmail.com>
2025-04-07 23:53:55 -0700

    SE CAMBIO PARA EL RENDER

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const __dirname = path.resolve();
 
 app.use(express.json()); 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.get("/apiV1/swagger.yaml", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "apiV1", "swagger.yaml"));
+  });
 app.use("/apiV1/usuarios", friendRequestRoutes);
 app.use("/apiV1/usuarios", usersRoutes);
 app.use("/apiV1/usuarios", progressRoutes);
